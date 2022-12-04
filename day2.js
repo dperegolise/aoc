f0c162aa3165e2d053bc1d2670684dfc5af1c3a4
Daniel Peregolise <danielperegolise@gmail.com>
2022-12-04 17:48:55 -0500

    day 2 part 2

--- day2.js
+++ day2.js
@@ -1,6 +1,9 @@
 import { getInput } from './aoc_util.js';
 
 const input = await getInput(2);
+const LOSE = 0;
+const TIE = 3;
+const WIN = 6;
 const key = new Map([
     ['X', 'A'],
     ['Y', 'B'],
@@ -12,10 +15,28 @@ const shapes = new Map([
     ['C', 3], // scissors
 ]);
 
-const score = input.split('\r\n')
-    .map(m => [shapes.get(m.substring(0,1)), shapes.get(key.get(m.substring(2)))])
+const scoreForKey = input.split('\r\n')
+    .map(m => [shapes.get(m.substring(0, 1)), shapes.get(key.get(m.substring(2)))])
     .reduce((acc, m) => {
-        return acc + m[1] + (m[0] - m[1] == 1 || m[0] - m[1] == -2 ? 0 : (m[0] == m[1] ? 3 : 6));
+        return acc + m[1] + (m[0] - m[1] == 1 || m[0] - m[1] == -2 ? LOSE : (m[0] == m[1] ? TIE : WIN));
     }, 0);
 
-console.log(score);
+console.log('Score if guide is a key ' + scoreForKey);
+
+const scoreForEnd = input.split('\r\n')
+    .map(m => [shapes.get(m.substring(0, 1)), m.substring(2)])
+    .reduce((acc, m) => {
+        let round;
+        switch (m[1]) {
+            case 'X': round = LOSE + (m[0] != 1 ? m[0] - 1 : shapes.get('C'));
+                break;
+            case 'Y': round = TIE + m[0];
+                break;
+            case 'Z': round = WIN + (m[0] != 3 ? m[0] + 1 : shapes.get('A'));
+                break;
+            default:
+        } 
+        return acc + round;
+    }, 0);
+
+console.log('Score if guide is a how to end ' + scoreForEnd);
